d946c46489680eeee203d70e2652979180a6ae4e
Choi D-H <109265266+choidahye99@users.noreply.github.com>
2024-02-05 15:53:19 +0900

    Title: 입력받아 계산, Time: 195ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240205/입력받아 계산/input-calculate.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 여기에 코드를 작성해주세요.
+
+        int ans = sc.nextInt();
+        System.out.println(ans+2);
+    }
+}
